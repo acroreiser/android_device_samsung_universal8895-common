45bc14bc2be6ed60077b0e1265c7373403fd0790
Roberto Sartori <roberto.sartori.android@gmail.com>
2024-12-10 22:51:10 +0100

    universal8895-common: Build libprotobuf-cpp-*-v29
    Provided by lineage-compat. Remove unavailable PRODUCT_COPY_FILES.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -344,12 +344,12 @@ VENDOR_SECURITY_PATCH := 2021-04-01
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:system/lib/libhidlbase-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:system/lib64/libhidlbase-v32.so \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so
+
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-v29 \
+    libprotobuf-cpp-lite-v29 \
 
 # Vibrator
 PRODUCT_PACKAGES += \
