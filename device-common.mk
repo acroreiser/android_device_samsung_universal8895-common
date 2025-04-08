4b75e7a70dec297c88996384aec1fd4dd7f8d4f9
pix106 <sbordenave@gmail.com>
2025-04-08 07:53:36 +0200

    universal8895-common: build libprocessgroup_shim

--- device-common.mk
+++ device-common.mk
@@ -318,7 +318,8 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     libaudioparams_shim \
     libexynoscamera_shim \
-    libexynosdisplay_shim
+    libexynosdisplay_shim \
+    libprocessgroup_shim
 
 # stagefright
 PRODUCT_PACKAGES += \
