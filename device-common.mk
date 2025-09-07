8d38d7128d996b1ad0fc7c02a72068ae748dd19c
Nicholas Magill <nmagill1979@gmail.com>
2025-09-07 21:15:32 +0100

    universal8895-common: Drop fastcharge HAL
    * Fastcharge HIDL has been deprecated

--- device-common.mk
+++ device-common.mk
@@ -122,10 +122,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_HOST_PACKAGES += \
     dtbhtoolExynos
 
-# FastCharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.samsung
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
