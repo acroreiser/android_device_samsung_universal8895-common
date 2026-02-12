e1a9bcb1f3ed875edba75dd64b20f487ca305d8b
Yaroslav Zviezda <acroreiser@gmail.com>
2026-02-12 16:56:02 -0500

    universal8895-common: downver android.hardware.graphics.composer to 2.1 to match stock

--- device-common.mk
+++ device-common.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     libion \
     libutilscallstack
