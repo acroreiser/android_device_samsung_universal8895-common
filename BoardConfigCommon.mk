39fbccfd01c21cf6eecca65018f9aa4d1f197d69
Yaroslav Zviezda <acroreiser@gmail.com>
2026-02-12 16:48:43 -0500

    universal8895-common: build kernel with GCC

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,8 +87,9 @@ TARGET_KERNEL_CLANG_COMPILE := false
 TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CLANG_VERSION := r416183b
-TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TOP_PATH := $(realpath $(TOP))
+KERNEL_TOOLCHAIN := $(TOP_PATH)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
 
 # Kernel config
 TARGET_KERNEL_SOURCE := kernel/samsung/universal8895
